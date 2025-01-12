97bff6480db1da4b389f6f3f1e3cb0479c6a0a48
Ess Ess Bee <ai8w14@gmail.com>
2025-01-12 09:33:24 -0500

    Update ProxMoxQuestionMarks.sh
    Added "echo" statements for status.

--- ProxMoxQuestionMarks.sh
+++ ProxMoxQuestionMarks.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 # Resets Proxmox services to get rid of the question marks and loss of sync in a cluster.
+echo "Stopping:"
+echo "     pve-cluster"
 service pve-cluster stop
+echo "     corosync"
 service corosync stop
+echo "     pvestatd"
 service pvestatd stop
+echo "     pveproxy"
 service pveproxy stop
+echo "     pvedaemon"
 service pvedaemon stop
+echo "Starting:"
+echo "     pve-cluster"
 service pve-cluster start
+echo "     corosync"
 service corosync start
+echo "     pvestatd"
 service pvestatd start
+echo "     pveproxy"
 service pveproxy start
+echo "     pvedaemon"
 service pvedaemon start
+echo "Now you pray it worked."
