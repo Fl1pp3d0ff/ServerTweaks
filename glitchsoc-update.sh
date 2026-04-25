8c0fc4e55b2e836737ce007785c901f170266571
AI8W - Chris <ai8w14@gmail.com>
2026-04-25 12:13:21 -0400

    Update glitchsoc-update.sh
    Created post-update script, so post-update instructions not needed at end of this script.
    This also better handles pulling new code from github than the previous version.

--- glitchsoc-update.sh
+++ glitchsoc-update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo ">>>>>>THIS SCRIPT SHOULD BE RUN IN YOUR MASTODON LIVE DIRECTORY<<<<<<"
+#echo "====================> Changing to LIVE dir."
+#cd live
 echo "====================> Stashing customizations."
 git stash
 echo "====================> Pulling new code."
+git fetch glitch-soc
 git pull glitch-soc main
 echo "====================> Restoring customizations."
 git stash pop
 echo "====================> Performing bundle and yarn installs."
 bundle install && yarn install
-echo "====================> Performing DB pre-migrate."
+echo "====================> Performing DB migrate."
 RAILS_ENV=production SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
 echo "====================> Precompiling assets."
 RAILS_ENV=production bundle exec rails assets:precompile
 echo "=========================================="
-echo "Restart services with:  systemctl reload mastodon-web && systemctl restart mastodon-{sidekiq,streaming}"
-echo "Then execute:           RAILS_ENV=production bin/tootctl cache clear"
-echo "Finally migrate the DB: RAILS_ENV=production bundle exec rails db:migrate"
+echo "Execute 'postupdate.sh' to complete the upgrade."
 echo "=========================================="
